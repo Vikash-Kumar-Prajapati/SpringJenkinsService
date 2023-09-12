a59eac271f41f5be1e57963108f55b3b6d33b7b9
vikashprajapati <vikash.prajapati@nagarro.com>
2023-09-12 22:41:21 +0530

    Added logger and basic test case

--- src/main/java/com/nagarro/vikashprajapati/SpringJenkinsService/SpringJenkinsServiceApplication.java
+++ src/main/java/com/nagarro/vikashprajapati/SpringJenkinsService/SpringJenkinsServiceApplication.java
@@ -1,13 +1,24 @@
 package com.nagarro.vikashprajapati.SpringJenkinsService;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJenkinsServiceApplication.class, args);
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsServiceApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started.........");
+
 	}
 
+	public static void main(String[] args) {
+		logger.info("Application Executing in Main method..........");
+		SpringApplication.run(SpringJenkinsServiceApplication.class, args);
+	}
 }
